17699f9076bdf4ec12059386ede4f22e9822d15d
Jordan DeLoach <jordan@jtmcgee.net>
2011-09-18 21:47:48 -0500

    added proj4

---
+++ JordanDeLoach.Cis300.DistanceFinder/JordanDeLoach.Cis300.DistanceFinder/DistanceFinder.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace JordanDeLoach.Cis300.DistanceFinder
+{
+    partial class uxDistancefinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.uxThresholdLabel = new System.Windows.Forms.Label();
+            this.uxPathColorLabel = new System.Windows.Forms.Label();
+            this.uxNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.uxColorDialog = new System.Windows.Forms.ColorDialog();
+            this.uxPanel = new System.Windows.Forms.Panel();
+            this.uxPictureBox = new System.Windows.Forms.PictureBox();
+            this.uxOpenFileButton = new System.Windows.Forms.Button();
+            this.uxOpenColorButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.uxNumericUpDown)).BeginInit();
+            this.uxPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.uxPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uxThresholdLabel
+            // 
+            this.uxThresholdLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.uxThresholdLabel.AutoSize = true;
+            this.uxThresholdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxThresholdLabel.Location = new System.Drawing.Point(127, 415);
+            this.uxThresholdLabel.Name = "uxThresholdLabel";
+            this.uxThresholdLabel.Size = new System.Drawing.Size(83, 20);
+            this.uxThresholdLabel.TabIndex = 0;
+            this.uxThresholdLabel.Text = "Threshold:";
+            // 
+            // uxPathColorLabel
+            // 
+            this.uxPathColorLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.uxPathColorLabel.AutoSize = true;
+            this.uxPathColorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxPathColorLabel.Location = new System.Drawing.Point(319, 415);
+            this.uxPathColorLabel.Name = "uxPathColorLabel";
+            this.uxPathColorLabel.Size = new System.Drawing.Size(87, 20);
+            this.uxPathColorLabel.TabIndex = 1;
+            this.uxPathColorLabel.Text = "Path Color:";
+            // 
+            // uxNumericUpDown
+            // 
+            this.uxNumericUpDown.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.uxNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxNumericUpDown.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.uxNumericUpDown.Location = new System.Drawing.Point(216, 411);
+            this.uxNumericUpDown.Maximum = new decimal(new int[] {
+            765,
+            0,
+            0,
+            0});
+            this.uxNumericUpDown.Name = "uxNumericUpDown";
+            this.uxNumericUpDown.Size = new System.Drawing.Size(82, 26);
+            this.uxNumericUpDown.TabIndex = 2;
+            this.uxNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.uxNumericUpDown.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // uxPanel
+            // 
+            this.uxPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxPanel.AutoScroll = true;
+            this.uxPanel.Controls.Add(this.uxPictureBox);
+            this.uxPanel.Location = new System.Drawing.Point(15, 14);
+            this.uxPanel.Name = "uxPanel";
+            this.uxPanel.Size = new System.Drawing.Size(454, 387);
+            this.uxPanel.TabIndex = 3;
+            // 
+            // uxPictureBox
+            // 
+            this.uxPictureBox.Location = new System.Drawing.Point(4, 4);
+            this.uxPictureBox.Name = "uxPictureBox";
+            this.uxPictureBox.Size = new System.Drawing.Size(0, 0);
+            this.uxPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.uxPictureBox.TabIndex = 0;
+            this.uxPictureBox.TabStop = false;
+            this.uxPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.uxPictureBox_MouseDown);
+            this.uxPictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.uxPictureBox_MouseUp);
+            // 
+            // uxOpenFileButton
+            // 
+            this.uxOpenFileButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.uxOpenFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxOpenFileButton.Location = new System.Drawing.Point(19, 408);
+            this.uxOpenFileButton.Name = "uxOpenFileButton";
+            this.uxOpenFileButton.Size = new System.Drawing.Size(86, 35);
+            this.uxOpenFileButton.TabIndex = 4;
+            this.uxOpenFileButton.Text = "Open File";
+            this.uxOpenFileButton.UseVisualStyleBackColor = true;
+            this.uxOpenFileButton.Click += new System.EventHandler(this.uxOpenFileButton_Click);
+            // 
+            // uxOpenColorButton
+            // 
+            this.uxOpenColorButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.uxOpenColorButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.uxOpenColorButton.Location = new System.Drawing.Point(412, 406);
+            this.uxOpenColorButton.Name = "uxOpenColorButton";
+            this.uxOpenColorButton.Size = new System.Drawing.Size(56, 37);
+            this.uxOpenColorButton.TabIndex = 5;
+            this.uxOpenColorButton.UseVisualStyleBackColor = false;
+            this.uxOpenColorButton.Click += new System.EventHandler(this.uxOpenColorButton_Click);
+            // 
+            // uxDistancefinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 458);
+            this.Controls.Add(this.uxOpenColorButton);
+            this.Controls.Add(this.uxOpenFileButton);
+            this.Controls.Add(this.uxPanel);
+            this.Controls.Add(this.uxNumericUpDown);
+            this.Controls.Add(this.uxPathColorLabel);
+            this.Controls.Add(this.uxThresholdLabel);
+            this.MinimumSize = new System.Drawing.Size(499, 496);
+            this.Name = "uxDistancefinder";
+            this.Text = "Distance Finder";
+            ((System.ComponentModel.ISupportInitialize)(this.uxNumericUpDown)).EndInit();
+            this.uxPanel.ResumeLayout(false);
+            this.uxPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.uxPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog uxOpenFileDialog;
+        private System.Windows.Forms.Label uxThresholdLabel;
+        private System.Windows.Forms.Label uxPathColorLabel;
+        private System.Windows.Forms.NumericUpDown uxNumericUpDown;
+        private System.Windows.Forms.ColorDialog uxColorDialog;
+        private System.Windows.Forms.Panel uxPanel;
+        private System.Windows.Forms.PictureBox uxPictureBox;
+        private System.Windows.Forms.Button uxOpenFileButton;
+        private System.Windows.Forms.Button uxOpenColorButton;
+    }
+}
+
---
+++ JordanDeLoach.Cis300.DistanceFinder/JordanDeLoach.Cis300.DistanceFinder/DistanceFinder.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace JordanDeLoach.Cis300.DistanceFinder
+{
+    public partial class uxDistancefinder : Form
+    {
+
+        private Point _startingPoint;
+        private Point _endPoint;
+        private Bitmap _image;
+        private Point[,] _predecessor;
+        private Point _empty = new Point(-1, 0);
+        private Point _noPredecessor = new Point(-2, 0);
+
+        public uxDistancefinder()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Constructs a new predecessor array with empty values
+        /// </summary>
+        /// <returns>A point array of value empty.</returns>
+        private Point[,] NewPredecessorArray()
+        {
+            Point[,] points = new Point[uxPictureBox.Width, uxPictureBox.Height];
+
+            for (int i = 0; i < _image.Width; i++)
+            {
+                for (int j = 0; j < _image.Height; j++)
+                {
+                    points[i, j] = _empty;
+                }
+            }
+
+            return points;
+        }
+
+        /// <summary>
+        /// Returns true if the pixel provided, and the starting point are similar in color, as defined in the NumericUpdown Threshold.
+        /// </summary>
+        /// <param name="x">X coordinate of point</param>
+        /// <param name="y">Y coordinate of point</param>
+        /// <returns>True if similar, false if not</returns>
+        private bool Similar(int x, int y)
+        {
+            Color startColor = _image.GetPixel(_startingPoint.X, _startingPoint.Y);
+            Color inputColor = _image.GetPixel(x, y);
+
+            int value = Math.Abs(startColor.R - inputColor.R) + Math.Abs(startColor.G - inputColor.G) + Math.Abs(startColor.B - inputColor.B);
+
+            if (value <= uxNumericUpDown.Value)
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Updates queue and array if it meets conditions
+        /// </summary>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        /// <param name="predecessor">Predecessor point</param>
+        /// <param name="queue">The point queue used</param>
+        private void Update(int x, int y, Point predecessor, Queue<Point> queue)
+        {
+            if (x <= _image.Width-1 && y <= _image.Height-1 && y >= 0 && x >= 0)
+                if (_predecessor[x, y].Equals(_empty))
+                    if (Similar(x, y))
+                    {
+                        queue.Enqueue(new Point(x, y));
+                        _predecessor[x, y] = predecessor;
+                    }
+        }
+
+        /// <summary>
+        /// Implements the Breadth-First search
+        /// </summary>
+        private void Search() 
+        {
+            _predecessor = NewPredecessorArray();
+            _predecessor[_startingPoint.X, _startingPoint.Y] = _noPredecessor;
+            
+            Queue<Point> queue = new Queue<Point>();
+            queue.Enqueue(_startingPoint);
+
+            while (queue.Count > 0)
+            {
+                Point test = queue.Dequeue();
+                if (test.Equals(_endPoint))
+                    break;
+                Update(test.X + 1, test.Y, test, queue);
+                Update(test.X, test.Y + 1, test, queue);
+                Update(test.X - 1, test.Y, test, queue);
+                Update(test.X, test.Y - 1, test, queue);
+            }
+            if (queue.Count == 0)
+                _predecessor = null;
+        }
+
+        /// <summary>
+        /// Draws the path from the queue.
+        /// </summary>
+        /// <returns></returns>
+        private int DrawPath()
+        {
+            int X = _endPoint.X;
+            int Y = _endPoint.Y;
+            int i = 0;
+
+            while (true)
+            {
+                _image.SetPixel(X, Y, uxColorDialog.Color);
+
+                if (!_predecessor[X, Y].Equals(_noPredecessor))
+                {
+                    int x = _predecessor[X, Y].X;
+                    Y = _predecessor[X, Y].Y;
+                    // its a little hack so that Y can be set properly with old X
+                    X = x;
+                }
+                else
+                {
+                    break;
+                }
+                i++;
+            }
+
+            uxPictureBox.Image = _image;
+            return i - 1;
+        }
+
+        /// <summary>
+        /// Handles a button click that opens the file dialog and attempts top open the image on the panel, handling exceptions as needed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxOpenFileButton_Click(object sender, EventArgs e)
+        {
+            if (uxOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Bitmap image = new Bitmap(uxOpenFileDialog.FileName);
+                    _image = new Bitmap(image.Width, image.Height, PixelFormat.Format32bppRgb);
+                    Graphics.FromImage(_image).DrawImage(image, 0, 0, image.Width, image.Height);
+                    uxPictureBox.Image = image;
+                    _predecessor = null;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + "\n" + ex.StackTrace, "Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+        /// <summary>
+        /// Handles a click to the button of the color selector.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxOpenColorButton_Click(object sender, EventArgs e)
+        {
+            if (uxColorDialog.ShowDialog() == DialogResult.OK)
+            {
+                uxOpenColorButton.BackColor = uxColorDialog.Color;
+                if (_predecessor != null)
+                    DrawPath();
+            }
+        }
+
+
+        /// <summary>
+        /// Saves the start part of the maze.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxPictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            _endPoint = e.Location;
+            Search();
+            if (_predecessor != null)
+                MessageBox.Show("Distance = " + DrawPath());
+            else
+                MessageBox.Show("No path exists between the specified points.");
+
+        }
+
+        /// <summary>
+        /// Handles the end click of the path
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxPictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            _startingPoint = e.Location;
+        }
+    }
+}
