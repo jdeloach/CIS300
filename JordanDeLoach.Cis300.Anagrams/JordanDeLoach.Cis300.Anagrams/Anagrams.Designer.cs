9ffe9194297fca2123bb1e63bfdc00ab18ad295a
Jordan DeLoach <jordan@jtmcgee.net>
2011-10-21 13:52:20 -0500

    finished proj6

---
+++ JordanDeLoach.Cis300.Anagrams/JordanDeLoach.Cis300.Anagrams/Anagrams.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace JordanDeLoach.Cis300.Anagrams
+{
+    partial class uxAnagrams
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxEnterStringLabel = new System.Windows.Forms.Label();
+            this.uxString = new System.Windows.Forms.TextBox();
+            this.uxFindAnagrams = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.uxListBox = new System.Windows.Forms.ListBox();
+            this.uxAnagramCount = new System.Windows.Forms.TextBox();
+            this.uxCountLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // uxEnterStringLabel
+            // 
+            this.uxEnterStringLabel.AutoSize = true;
+            this.uxEnterStringLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uxEnterStringLabel.Location = new System.Drawing.Point(13, 13);
+            this.uxEnterStringLabel.Name = "uxEnterStringLabel";
+            this.uxEnterStringLabel.Size = new System.Drawing.Size(98, 20);
+            this.uxEnterStringLabel.TabIndex = 0;
+            this.uxEnterStringLabel.Text = "Enter String:";
+            // 
+            // uxString
+            // 
+            this.uxString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxString.Location = new System.Drawing.Point(13, 37);
+            this.uxString.Name = "uxString";
+            this.uxString.Size = new System.Drawing.Size(180, 20);
+            this.uxString.TabIndex = 1;
+            // 
+            // uxFindAnagrams
+            // 
+            this.uxFindAnagrams.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxFindAnagrams.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxFindAnagrams.Location = new System.Drawing.Point(12, 63);
+            this.uxFindAnagrams.Name = "uxFindAnagrams";
+            this.uxFindAnagrams.Size = new System.Drawing.Size(180, 45);
+            this.uxFindAnagrams.TabIndex = 2;
+            this.uxFindAnagrams.Text = "Find Anagrams";
+            this.uxFindAnagrams.UseVisualStyleBackColor = true;
+            this.uxFindAnagrams.Click += new System.EventHandler(this.uxFindAnagrams_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Anagrams:";
+            // 
+            // uxListBox
+            // 
+            this.uxListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxListBox.FormattingEnabled = true;
+            this.uxListBox.Location = new System.Drawing.Point(13, 139);
+            this.uxListBox.Name = "uxListBox";
+            this.uxListBox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.uxListBox.Size = new System.Drawing.Size(180, 264);
+            this.uxListBox.TabIndex = 4;
+            // 
+            // uxAnagramCount
+            // 
+            this.uxAnagramCount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxAnagramCount.Location = new System.Drawing.Point(12, 431);
+            this.uxAnagramCount.Name = "uxAnagramCount";
+            this.uxAnagramCount.ReadOnly = true;
+            this.uxAnagramCount.Size = new System.Drawing.Size(180, 20);
+            this.uxAnagramCount.TabIndex = 5;
+            // 
+            // uxCountLabel
+            // 
+            this.uxCountLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.uxCountLabel.AutoSize = true;
+            this.uxCountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxCountLabel.Location = new System.Drawing.Point(8, 408);
+            this.uxCountLabel.Name = "uxCountLabel";
+            this.uxCountLabel.Size = new System.Drawing.Size(164, 20);
+            this.uxCountLabel.TabIndex = 6;
+            this.uxCountLabel.Text = "Number of Anagrams:";
+            // 
+            // uxAnagrams
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 463);
+            this.Controls.Add(this.uxCountLabel);
+            this.Controls.Add(this.uxAnagramCount);
+            this.Controls.Add(this.uxListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uxFindAnagrams);
+            this.Controls.Add(this.uxString);
+            this.Controls.Add(this.uxEnterStringLabel);
+            this.MinimumSize = new System.Drawing.Size(221, 501);
+            this.Name = "uxAnagrams";
+            this.Text = "Anagrams";
+            this.Load += new System.EventHandler(this.uxAnagrams_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label uxEnterStringLabel;
+        private System.Windows.Forms.TextBox uxString;
+        private System.Windows.Forms.Button uxFindAnagrams;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox uxListBox;
+        private System.Windows.Forms.TextBox uxAnagramCount;
+        private System.Windows.Forms.Label uxCountLabel;
+    }
+}
+
---
+++ JordanDeLoach.Cis300.Anagrams/JordanDeLoach.Cis300.Anagrams/Anagrams.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using RodHowell.Cis300.ConsList;
+
+namespace JordanDeLoach.Cis300.Anagrams
+{
+    public partial class uxAnagrams : Form
+    {
+        /// <summary>
+        /// Number of words in dictionary
+        /// </summary>
+        private const int NumberWords = 81539;
+
+        /// <summary>
+        /// File name of the dictionary to open
+        /// </summary>
+        private const String WordFile = "dictionary.txt";
+
+        /// <summary>
+        /// Words loaded from dictionary
+        /// </summary>
+        private string[] _wordList;
+
+        public uxAnagrams()
+        {
+            InitializeComponent();
+            ReadList();
+        }
+
+        /// <summary>
+        /// Reads the file into the array
+        /// </summary>
+        private void ReadList()
+        {
+            _wordList = new string[NumberWords];
+
+            try
+            {
+                using (StreamReader input = File.OpenText(WordFile))
+                    for (int i = 0; i < _wordList.Length; i++)
+                        _wordList[i] = input.ReadLine().Trim();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message + "\n" + e.StackTrace,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Builds a Queue of chars from the given string
+        /// </summary>
+        /// <param name="s">String to generate queue from</param>
+        /// <returns>queue of chars from given string</returns>
+        private Queue<char> BuildQueue(string s)
+        {
+            return new Queue<char>(s);
+        }
+
+        /// <summary>
+        /// Builds a string builder from a cons list
+        /// </summary>
+        /// <param name="s">ConsList of prefix's</param>
+        /// <returns>A stringbuilder with spaces seperating prefixes</returns>
+        private StringBuilder BuildStringBuilder(ConsList<string> s)
+        {
+            StringBuilder st = new StringBuilder();
+            while (s != null)
+            {
+                st.Append(s.Head + " ");
+                s = s.Tail;
+            }
+            return st;
+        }
+        
+        /// <summary>
+        /// Compares a string builder to a string for equality
+        /// </summary>
+        /// <param name="builder">String builder used to compare</param>
+        /// <param name="s">String used to compare</param>
+        /// <returns>Negative if builder is shorter, positive if builder is longer, 0 if equal.</returns>
+        private int CompareStringBuilder(StringBuilder builder, string s)
+        {
+            char[] builderArray = builder.ToString().ToCharArray();
+            char[] stringArray = s.ToCharArray();
+
+            int shorter = Math.Min(builder.Length, s.Length);
+
+            for (int i = 0; i < shorter; i++)
+            {
+                if (builderArray[i] != stringArray[i])
+                    return builderArray[i] - stringArray[i];
+            }
+
+            return builder.Length - s.Length;
+        }
+
+        /// <summary>
+        /// Gets anagrams with certain permutations
+        /// </summary>
+        /// <param name="prefixComplete">A phrase of complete words to be used a prefix; </param>
+        /// <param name="prefixPart">A prefix of a word to follow the above phrase; and </param>
+        /// <param name="text">The characters from which each anagram will be completed. </param>
+        private void GetAnagrams(ConsList<string> prefixComplete, StringBuilder prefixPart, Queue<char> text)
+        {
+            if (text.Count == 0)
+            {
+                if (ContainsWord(prefixPart)) 
+                {
+                    prefixComplete = new ConsList<string>(prefixPart.ToString(), prefixComplete);
+                    uxListBox.Items.Add(BuildStringBuilder(prefixComplete).ToString());
+                }
+                else if (prefixPart.Length == 0)
+                {
+                    uxListBox.Items.Add(BuildStringBuilder(prefixComplete).ToString());
+                }
+            }
+            else
+            {
+                if(ContainsWord(prefixPart))
+                    GetAnagrams(new ConsList<string>(prefixPart.ToString(), prefixComplete), new StringBuilder(""), text);
+                for (int i = 0; i < text.Count; i++)
+                {
+                    char character = text.Dequeue();
+                    GetAnagrams(prefixComplete, prefixPart.Append(character), text);
+                    text.Enqueue(character);
+                    prefixPart.Remove(prefixPart.Length - 1, 1);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Does a binary search for the word
+        /// </summary>
+        /// <param name="word">Word to find</param>
+        /// <returns>True/False depending on if it was found</returns>
+        private bool ContainsWord(StringBuilder word)
+        {
+            int start = 0;
+            int end = _wordList.Length;
+
+            while (start < end)
+            {
+                int mid = (start + end) / 2;
+
+                if (CompareStringBuilder(word, _wordList[mid]) > 0)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid; 
+                }
+            }
+
+            if (0 <= start && start <= _wordList.Length-1)
+                if (_wordList[start] == word.ToString())
+                    return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Handles the event when the search button is hit
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxFindAnagrams_Click(object sender, EventArgs e)
+        {
+            uxListBox.Items.Clear();
+            uxString.Text = uxString.Text.ToLower();
+            GetAnagrams(new ConsList<string>("", null), new StringBuilder(""), BuildQueue(uxString.Text.Trim()));
+            //if(ContainsWord(new StringBuilder(uxString.Text)))
+            //uxListBox.Items.Add(CompareStringBuilder(new StringBuilder("rawr"), uxString.Text));    
+                //uxListBox.Items.Add("Finished");
+            uxAnagramCount.Text = "" + uxListBox.Items.Count;         
+        }
+        
+        /// <summary>
+        /// Deals with a failure to load file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxAnagrams_Load(object sender, EventArgs e)
+        {
+            if (_wordList[_wordList.Length - 1] == null)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
